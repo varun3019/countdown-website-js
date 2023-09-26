7f19d7f462ac06db62cff13f73f6d142ff9d2a64
varun3019 <varunrao1000@gmail.com>
2023-09-26 13:23:20 +0530

    first commit

---
+++ countdown timer/App.js
@@ -0,0 +1,27 @@
+const endDate = "26 Sept 2023 17:29"
+
+document.getElementById('endDate').innerHTML = endDate;
+const inputs = document.querySelectorAll("input")
+
+function clock()
+{
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now)/1000;  
+    if (diff <0) return; 
+    inputs[0].value = Math.floor(diff/ 3600/ 24);
+    inputs[1].value = Math.floor(diff/ 3600) % 24;
+    inputs[2].value = Math.floor(diff/ 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+    //CONVERT INTO DATES
+
+}
+
+clock()
+
+setInterval(
+    () =>{
+        clock()
+    },
+    1000
+)
